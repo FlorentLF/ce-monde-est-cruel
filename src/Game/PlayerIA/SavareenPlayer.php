3a790e58b55af94b79fe96df81e903d29d7b267e
FlorentLF <florent.le-francois@epita.fr>
2020-10-07 20:29:32 +0200

    first try

--- src/Game/PlayerIA/SavareenPlayer.php
+++ src/Game/PlayerIA/SavareenPlayer.php
@@ -7,42 +7,33 @@ use Hackathon\Game\Result;
 /**
  * Class SavareenPlayers
  * @package Hackathon\PlayerIA
- * @author YOUR NAME HERE
+ * @author FLORENT LE FRANCOIS
  */
 class SavareenPlayer extends Player
 {
     protected $mySide;
     protected $opponentSide;
     protected $result;
-
+    
+    private function playopposite(string $ennemy)
+    {
+        if ($ennemy == "scissors")
+            return parent::rockChoice();
+        if ($ennemy == "rock")
+            return parent::paperChoice();
+        return parent::scissorsChoice();
+    }
     public function getChoice()
     {
-        // -------------------------------------    -----------------------------------------------------
-        // How to get my Last Choice           ?    $this->result->getLastChoiceFor($this->mySide) -- if 0 (first round)
-        // How to get the opponent Last Choice ?    $this->result->getLastChoiceFor($this->opponentSide) -- if 0 (first round)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get my Last Score            ?    $this->result->getLastScoreFor($this->mySide) -- if 0 (first round)
-        // How to get the opponent Last Score  ?    $this->result->getLastScoreFor($this->opponentSide) -- if 0 (first round)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get all the Choices          ?    $this->result->getChoicesFor($this->mySide)
-        // How to get the opponent Last Choice ?    $this->result->getChoicesFor($this->opponentSide)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get my Last Score            ?    $this->result->getLastScoreFor($this->mySide)
-        // How to get the opponent Last Score  ?    $this->result->getLastScoreFor($this->opponentSide)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get the stats                ?    $this->result->getStats()
-        // How to get the stats for me         ?    $this->result->getStatsFor($this->mySide)
-        //          array('name' => value, 'score' => value, 'friend' => value, 'foe' => value
-        // How to get the stats for the oppo   ?    $this->result->getStatsFor($this->opponentSide)
-        //          array('name' => value, 'score' => value, 'friend' => value, 'foe' => value
-        // -------------------------------------    -----------------------------------------------------
-        // How to get the number of round      ?    $this->result->getNbRound()
-        // -------------------------------------    -----------------------------------------------------
-        // How can i display the result of each round ? $this->prettyDisplay()
-        // -------------------------------------    -----------------------------------------------------
-
-
-        return parent::rockChoice();
-
+        if ($this->result->getNbRound() <= 1)
+        {
+            if ($this->result->getLastChoiceFor($this->opponentSide) == 0){
+                return parent::rockChoice();
+            }
+            else{
+                return playopposite($this->result->getLastChoiceFor($this->opponentSide));
+            }
+        }
+        return playopposite($this->result->getLastChoiceFor($this->opponentSide));
     }
 };
